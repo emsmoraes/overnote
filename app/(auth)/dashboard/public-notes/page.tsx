e1db8d12c15f430f783906858a0721c89542cd2b
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-01-29 22:06:21 -0300

    feat: add public notes page

--- app/(auth)/dashboard/public-notes/page.tsx
+++ app/(auth)/dashboard/public-notes/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import EmptyResults from "@/components/empty-results";
+import FeedItem from "@/components/feed-item";
+import NotesSkeleton from "@/components/notes-skeleton";
+import { listNotes } from "@/services/notes";
+import React, { Suspense } from "react";
 
-function page() {
+async function page() {
+  const notes = await listNotes();
+
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4 pt-0">page</div>
-  )
+    <Suspense fallback={<NotesSkeleton count={10} />}>
+      {notes.length === 0 && (
+        <EmptyResults />
+      )}
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        {notes.map((note) => (
+          <FeedItem
+            note={note}
+            key={note.id}
+            isAuthor={false}
+          />
+        ))}
+      </div>
+    </Suspense>
+  );
 }
 
-export default page
+export default page;
