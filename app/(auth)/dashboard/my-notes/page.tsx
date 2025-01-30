cb3a5ad0cb5fe7063557049f3f87760d27935832
emsmoraes <eduardomoraesdeveloper@gmail.com>
2025-01-29 21:46:56 -0300

    feat: use empty results component in my notes page

--- app/(auth)/dashboard/my-notes/page.tsx
+++ app/(auth)/dashboard/my-notes/page.tsx
@@ -1,3 +1,4 @@
+import EmptyResults from "@/components/empty-results";
 import FeedItem from "@/components/feed-item";
 import NotesSkeleton from "@/components/notes-skeleton";
 import { auth } from "@/lib/auth";
@@ -15,6 +16,8 @@ async function page() {
 
   return (
     <Suspense fallback={<NotesSkeleton count={10} />}>
+      {notes.length === 0 && <EmptyResults />}
+      
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         {notes.map((note) => (
           <FeedItem
